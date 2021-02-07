b4578024fbe88c10e4a850c2cd7ad5b1a177bd09
rbodi <rastislav.bodi@mtrust.eu>
2021-02-07 11:47:14 +0100

    basic functionality

--- Hlasky/ContentView.swift
+++ Hlasky/ContentView.swift
@@ -10,23 +10,42 @@ import SwiftUI
 struct ContentView: View {
 
   @State private var currentLetter = DataModel.random()
-  @State private var selection = TypHlasky.samohlaska
+  @State private var typSelection = TypHlasky.samohlaska
+  @State private var podtypSelection = PodtypHlasky.none
+
+  @State private var wrongCount = 0
 
   var body: some View {
     VStack {
       Text(currentLetter.znak)
-        //.font(.system(size: 140, weight: .bold))
-      Picker("typ", selection: $selection) {
+        .font(.system(size: 140, weight: .bold))
+      Picker("typ", selection: $typSelection) {
         Text("Samo").tag(TypHlasky.samohlaska)
         Text("Spolu").tag(TypHlasky.spoluhlaska)
         Text("Dvoj").tag(TypHlasky.dvojhlaska)
       }
+      Picker("subtyp", selection: $podtypSelection) {
+        if typSelection == .spoluhlaska {
+          Text("Tvrda").tag(PodtypHlasky.tvrda)
+          Text("Makka").tag(PodtypHlasky.makka)
+          Text("Obojaka").tag(PodtypHlasky.obojaka)
+        }
+        if typSelection == .samohlaska {
+          Text("Kratka").tag(PodtypHlasky.kratka)
+          Text("Dlha").tag(PodtypHlasky.dlha)
+        }
+      }
       Button("Dalej", action: next)
+      Text("Nespravne odpovede: \(wrongCount)")
     }.pickerStyle(SegmentedPickerStyle())
   }
 
   func next() {
-    currentLetter = DataModel.random()
+    if currentLetter.je(typu: typSelection, podtypSelection) {
+      currentLetter = DataModel.random()
+    } else {
+      wrongCount += 1
+    }
   }
 }
 
--- Hlasky/model/Pismeno.swift
+++ Hlasky/model/Pismeno.swift
@@ -11,4 +11,11 @@ struct Pismeno {
   let znak: String
   let typ: TypHlasky
   let podtyp: PodtypHlasky
+
+  func je(typu typ: TypHlasky, _ podtyp: PodtypHlasky) -> Bool {
+    if self.podtyp == .none {
+      return self.typ == typ
+    }
+    return self.typ == typ && self.podtyp == podtyp
+  }
 }
