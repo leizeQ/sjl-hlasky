d65c8d838e5f3615120a3b84d32e610984dfb812
rbodi <rastislav.bodi@mtrust.eu>
2021-02-07 09:12:08 +0100

    Initial Commit

---
+++ Hlasky/HlaskyApp.swift
@@ -0,0 +1,17 @@
+//
+//  HlaskyApp.swift
+//  Hlasky
+//
+//  Created by Rastislav Bodi on 07/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct HlaskyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
